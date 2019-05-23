07a63e67b3e51ec0754f9ca00cee121bc86c012b
Patrick Krause <pkpkrause@gmail.com>
2019-05-23 14:03:51 +0200

    Menü in erste Methoden ausgelagert und erweitert

--- src/Kontoverwaltung/Verwaltung.java
+++ src/Kontoverwaltung/Verwaltung.java
@@ -33,43 +33,34 @@ public class Verwaltung {
         System.out.println("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-");
         System.out.println();
 
-        if(Konten.isEmpty()){
-            System.out.println("Keine Konten vorhanden!");
-            System.out.println("Drücken Sie 'N' um ein neues Konto anzulegen.");
-        }
-        else{
-            System.out.println("Kontoliste:");
-            for (Konto lKonto : Konten) {
-                System.out.println(lKonto.Kontonummer + " [" + lKonto.Inhaber + "]");
-            }
-        }
+        // Menü aufbauen
+       showMenu();
 
 
-        Scanner in = new Scanner(System.in);
-        String s = in.nextLine();
-        System.out.println("Gewählter Buchstabe war" + s);
+
+
 
 
         // -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
         // Testweise Konten erstellen und einfügen
         // -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
 
-        //Girokonto konto1 = new Girokonto("Patrick Krause", 50.1389, 1000.0);
-        Girokonto konto1 = (Girokonto)Konten.get(0);
-        konto1.Auszahlung(20);
-
-        Sparkonto konto2 = new Sparkonto("Max Mustermann", 112.66, 1.2);
-        // Format yyyMMdd
-        konto2.setEroeffnungsdatum(LocalDate.of(2018,5,10));
-
-        Konten.add(konto2);
-
-        //konto2.berechneZinsen();
-        System.out.println("neuer Kontostand nach Zinsen: " + konto2.getSaldo() + "€");
-
-        konto2.ueberweisung(konto1, 30);
-
-        konto2.kontoauszug();
+//        //Girokonto konto1 = new Girokonto("Patrick Krause", 50.1389, 1000.0);
+//        Girokonto konto1 = (Girokonto)Konten.get(0);
+//        konto1.Auszahlung(20);
+//
+//        Sparkonto konto2 = new Sparkonto("Max Mustermann", 112.66, 1.2);
+//        // Format yyyMMdd
+//        konto2.setEroeffnungsdatum(LocalDate.of(2018,5,10));
+//
+//        Konten.add(konto2);
+//
+//        //konto2.berechneZinsen();
+//        System.out.println("neuer Kontostand nach Zinsen: " + konto2.getSaldo() + "€");
+//
+//        konto2.ueberweisung(konto1, 30);
+//
+//        konto2.kontoauszug();
 
         // -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
         // Ende des Tests
@@ -137,6 +128,101 @@ public class Verwaltung {
             System.out.println(e.getMessage());
         }
 
+    }
+
+
+    private static void showMenu(){
+        System.out.println("1 Konten ansehen");
+        System.out.println("2 Neues Konto anlegen");
+        System.out.println("3 Konto bearbeiten");
+        System.out.println("4 Konto löschen");
+        System.out.println();
+        System.out.println("5 Programm beenden");
+
+        // Eingaben auswerten
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        // Anzeige leeren
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+
+        switch(s) {
+
+            case("1"):
+            case("K"):
+            case("k"):
+                System.out.println("Kontoliste anzeigen");
+                showList();
+                break;
+            case("2"):
+            case("N"):
+            case("n"):
+                System.out.println("Neues Konto anlegen");
+                showNewDialog();
+                break;
+            case("3"):
+            case("B"):
+            case("b"):
+                System.out.println("Konto bearbeiten");
+               // showEditDialog();
+                break;
+            case("4"):
+            case("L"):
+            case("l"):
+                System.out.println("Konto löschen");
+               // showDeleteDialog();
+                break;
+            case("5"):
+            case("E"):
+            case("e"):
+                System.exit(0);
+            default:
+                System.out.println("Eingabe nicht erkannt!");
+                System.out.println("Beliebige Einagbe um zum Menü zurückzukehren...");
+                String e = in.nextLine();
+                showMenu();
+                break;
+        }
+    }
+
+    private static void showList(){
+
+        // Anzeige leeren
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+
+        if(Konten.isEmpty()){
+            System.out.println("Keine Konten vorhanden!");
+            System.out.println("Drücken Sie 'N' um ein neues Konto anzulegen.");
+
+            // Eingaben auswerten
+            Scanner in = new Scanner(System.in);
+            String e = in.nextLine();
+            if(e == "N" || e == "n"){
+                showNewDialog();
+            }
+            else{
+                showMenu();
+            }
+        }
+        else{
+            System.out.println("Kontoliste:");
+            for (Konto lKonto : Konten) {
+                System.out.println(lKonto.Kontonummer + " [" + lKonto.Inhaber + "]");
+            }
+        }
+    }
+
+    private static void showNewDialog(){
+
+        // Anzeige leeren
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+
+        System.out.println("Kontotyp auswählen:");
+        System.out.println("1 Girokonto");
+        System.out.println("2 Sparkonto");
     }
 
 }
